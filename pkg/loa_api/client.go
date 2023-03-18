46e9c42f69e89de01fb22cfcf7cd20e838269d6e
Jaemin Seo <jm199seo@gmail.com>
2023-03-18 18:14:42 +0900

    fix: revamp char level watcher

--- pkg/loa_api/client.go
+++ pkg/loa_api/client.go
@@ -28,7 +28,7 @@ type Client struct {
 func NewClient(cfg Config) (*Client, error) {
 	c := retryablehttp.NewClient()
 	c.RetryMax = 3
-	c.RetryWaitMin = 1 * time.Second
+	c.RetryWaitMin = 10 * time.Microsecond
 	c.RetryWaitMax = 5 * time.Second
 	c.Backoff = retryablehttp.LinearJitterBackoff
 	c.Logger = nil
@@ -50,12 +50,12 @@ func NewClient(cfg Config) (*Client, error) {
 	}
 	c.CheckRetry = retryPolicy(apiKeys)
 
+	// lostark API SSL certificate is invalid. so we need to skip verification
+	c.HTTPClient.Transport = http.DefaultTransport.(*http.Transport).Clone()
+	c.HTTPClient.Transport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
 	stdClient := c.StandardClient()
 	stdClient.Timeout = 15 * time.Second
-	// lostark API SSL certificate is invalid. so we need to skip verification
-	customTransport := stdClient.Transport.(*retryablehttp.RoundTripper).Client.HTTPClient.Transport.(*http.Transport).Clone()
-	customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	stdClient.Transport = customTransport
 
 	return &Client{
 		client:  stdClient,
@@ -88,9 +88,9 @@ func (c *Client) do(req *http.Request) (*http.Response, error) {
 
 	req.Header.Add("Authorization", "Bearer "+currentApiKey.Key)
 
-	if currentApiKey.Remaining == 0 && time.Now().Before(currentApiKey.Reset) {
-		time.Sleep(time.Until(currentApiKey.Reset))
-	}
+	// if currentApiKey.Remaining == 0 && time.Now().Before(currentApiKey.Reset) {
+	// 	time.Sleep(time.Until(currentApiKey.Reset))
+	// }
 
 	res, err := c.client.Do(req)
 	if err != nil {
@@ -109,6 +109,9 @@ func updateAPIMetadata(apiKey *APIKey, resp *http.Response) {
 	}
 	if reset, err := strconv.ParseInt(resp.Header.Get("X-RateLimit-Reset"), 10, 64); err == nil {
 		apiKey.Reset = time.Unix(reset, 0)
+	}
+	if retryAfter, err := strconv.ParseInt(resp.Header.Get("Retry-After"), 10, 64); err == nil {
+		apiKey.Reset = time.Now().Add(time.Second * time.Duration(retryAfter))
 	}
 }
 
